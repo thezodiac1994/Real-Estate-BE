2c631b0247c60bbaf68e5068f7fbc4ef3624c4ee
thezodiac1994 <gautamtrueblue@gmail.com>
2016-09-11 17:38:13 +0530

    Final input for 3 params is ready
    Added formatting coded for ip and created ip

---
+++ Scraping/Data/ip_final.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map <int,double> Airport;
+map <int,double> Altamount;
+vector <pair <int,int>> TP;
+
+int main () {
+
+    ifstream Alta ("AltamountDistance.txt");
+    int id,q,p;
+    double d;
+
+    while(Alta >> id){
+        Alta >> d;
+        Altamount[id] = d;
+        //cout << d << endl;
+    }
+
+    ifstream Air ("AirportDistance.txt");
+
+    while(Air >> id){
+        Air >> d;
+        Airport[id] = d;
+        //cout << d << endl;
+    }
+
+    ifstream TP("TimevsPrice.txt");
+    ofstream IP("ip.txt");
+
+    while(TP >> id){
+        TP >> q >> p;
+        IP << q << " " << Airport[id] << " " << Altamount[id] << " " << p << endl;
+    }
+
+    return 0;
+}
