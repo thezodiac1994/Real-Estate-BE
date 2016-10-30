978834d2820c6833b2472b6d44843b5c8ee8809f
darklord1995 <sampath.shetty15@gmail.com>
2016-10-30 19:45:17 +0530

    Updated

--- Scraping/Data/interface.cpp
+++ Scraping/Data/interface.cpp
@@ -6,6 +6,7 @@ map <int,double> Airport;
 map <int,double> Altamount,Vashi,Virar;
 map <int,int> Mallc;
 map <int, string> names;
+map <pair <int,int>,double> price;
 
 vector <pair <int,int>> TP;
 vector <double> theta;
@@ -91,22 +92,46 @@ int main () {
         cout << id << " " << s << "\n";
     }
 
+    ifstream TP("TimevsPrice.txt");
+
+    while(TP >> id){
+        TP >> q >> p;
+        price[make_pair(id,q)] = p;
+    }
+
     cout << "enter the area id (1-355)\n";
     int ID,time = 30;
     while(ID){
         time = 30;
         cin >> ID;
-        cout << "Gradient Descent price estimate for the coming quarters\n";
+        //cout << "Gradient Descent price estimate for the coming quarters\n";
+
+        double init = price[make_pair(ID,29)];
+        double temp_malls,temp_airport;
+        double factor = price[make_pair(ID,29)]/price[make_pair(ID,28)];
+
+        cout << "\nInput for dynamic factors\n";
+        cout << "Enter the number of upcoming shopping complexes/malls in the locality (0 if none)\n";
+        cin >> temp_malls;
+        cout << "Is there a new airport being constructed nearby to the locality, enter its distance from locality if true, if not enter -1\n";
+        cin >> temp_airport;
+        if(temp_airport==-1)
+            temp_airport = Airport[ID];
+
+        init = init + max(0.0,(Airport[ID]-temp_airport))*41.8322 + 18*temp_malls;
+        cout << names[ID] << endl;
         while(time<35){
-            double ans = theta[0] * time;
+            double val_1 = init*factor , val_2 = init + theta[0];
+            init = (val_1 + val_2)/2;
             //cout << ans << " ";
-            for(int i=1;i<=5;i++){
-                ans = ans + mat[ID][i] * theta[i];
+
+            //for(int i=1;i<=5;i++){
+              //  ans = ans + mat[ID][i] * theta[i];
                 //if(time==30)
                 //cout << endl << theta[i] << " " << mat[ID][i] << endl;
 
-            }
-            cout << ans+2000<< endl;
+
+            cout << init << endl;
             time++;
         }
     }
